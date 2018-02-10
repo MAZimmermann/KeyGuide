a059dfe67c2db00ade97cff8f8912bcce1c686c0
MAZimmermann <MAZimmermann26@gmail.com>
2018-02-09 19:40:37 -0500

    commented code, added TODOs, edited gitignore

--- main.py
+++ main.py
@@ -7,21 +7,25 @@ Created on Fri Feb  9 14:58:14 2018
 
 import requests
 
+# Grab username, clientId, and clientSecret
 with open('credentials.txt', 'r') as f:
     username = str.replace(f.readline(),'\n','')
     clientId = str.replace(f.readline(),'\n','')
-    clientSecret = str.replace(f.readline(),'\n','')
-    redirect = str.replace(f.readline(),'\n','')
-    
+    clientSecret = str.replace(f.readline(),'\n','')    
     f.close()
 
 def main():
     
+    # define base url
     baseURL = 'https://accounts.spotify.com/api/token'
+    
+    # define body parameters
     bodyParams = {'grant_type' : 'client_credentials'}
     
     appReq = requests.post(baseURL, data=bodyParams, auth=(clientId, clientSecret))
     print(appReq)
     print(appReq._content)
+
+    # TODO: Define scope, pull key from song data
 
 main()
